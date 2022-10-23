53c21caccdf6e14108a9892fc0b148faa65fbb61
Anurag301 <115408889+Anurag301@users.noreply.github.com>
2022-10-23 20:11:59 +0530

    Add files via upload

---
+++ assignment 2 part1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+def last(p):
+	return p[-1]
+
+def sort(tuple):
+	return sorted(tuple, key=last)
+
+b= [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
+print('sorted:')
+print(sort(b))
+
